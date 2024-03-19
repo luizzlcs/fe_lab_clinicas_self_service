7468086bcca2f50d9c6569adaee335162bc55355
Luizzlcs <luizzlcs@gmail.com>
2024-03-19 16:18:01 -0300

    Implmentando menu e tela inicial

--- lib/src/modules/home/home_page.dart
+++ lib/src/modules/home/home_page.dart
@@ -1,3 +1,6 @@
+import 'dart:ffi';
+
+import 'package:fe_lab_clinicas_core/fe_lab_clinicas_core.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,11 +8,53 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final sizeOf = MediaQuery.sizeOf(context);
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home page'),
+      appBar: LabClinicasAppBar(actions: [
+        PopupMenuButton<int>(
+          child: IconPopupMenuWidget(),
+          itemBuilder: (context) {
+            return [
+              PopupMenuItem<int>(
+                value: 1,
+                child: Text('Iniciar Terminal'),
+              ),
+              PopupMenuItem<int>(
+                value: 2,
+                child: Text('Finalizar Terminal'),
+              ),
+            ];
+          },
+        )
+      ]),
+      body: Align(
+        alignment: Alignment.center,
+        child: Container(
+          margin: EdgeInsets.only(bottom: 260),
+          padding:  EdgeInsets.all(40),
+          width: sizeOf.width * .8,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(16),
+              border: Border.all(color: LabClinicasTheme.orangeColor)),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                "Bem Vindo!",
+                style: LabClinicasTheme.titleStyle,
+              ),
+              const SizedBox(
+                height: 32,
+              ),
+              SizedBox(
+                width: sizeOf.width * .8,
+                child: ElevatedButton(onPressed: (){}, child: Text('INICIAR TERMINAL')),
+              ),
+            ],
+          ),
+        ),
       ),
-      body: Container(),
     );
   }
 }
