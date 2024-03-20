be368e670f271e44666d4f743f5053a483f288d2
Luizzlcs <luizzlcs@gmail.com>
2024-03-20 15:43:10 -0300

    Contruindo a primeira tela

---
+++ lib/src/modules/self_service/who_i_am/who_i_am_page.dart
@@ -0,0 +1,144 @@
+import 'package:fe_lab_clinicas_core/fe_lab_clinicas_core.dart';
+import 'package:fe_lab_clinicas_self_service_cb/src/modules/self_service/self_service_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_getit/flutter_getit.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:validatorless/validatorless.dart';
+
+class WhoIAmPage extends StatefulWidget {
+  const WhoIAmPage({super.key});
+
+  @override
+  State<WhoIAmPage> createState() => _WhoIAmPageState();
+}
+
+class _WhoIAmPageState extends State<WhoIAmPage> {
+  final selfServiceController = Injector.get<SelfServiceController>();
+
+  final formKey = GlobalKey<FormState>();
+  final firstNameEC = TextEditingController();
+  final lastNameEC = TextEditingController();
+
+  @override
+  void dispose() {
+    firstNameEC.dispose();
+    lastNameEC.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PopScope(
+      canPop: false,
+      onPopInvoked: (didPop) {
+        firstNameEC.text = '';
+        lastNameEC.text = '';
+        selfServiceController.clearForm();
+        
+      },
+      child: Scaffold(
+        appBar: LabClinicasAppBar(
+          actions: [
+            PopupMenuButton(
+                child: IconPopupMenuWidget(),
+                itemBuilder: (context) {
+                  return [
+                    const PopupMenuItem(
+                      value: 1,
+                      child: Text(' Finalizar terminal'),
+                    )
+                  ];
+
+                },
+                onSelected: (value)async{
+                  if(value == 1){
+                    final nav = Navigator.of(context);
+                     await SharedPreferences.getInstance().then((sp) => sp.clear());
+                     nav.pushNamedAndRemoveUntil('/', (route) => false);
+                  }
+                } 
+                ),
+            
+          ],
+        ),
+        body: LayoutBuilder(builder: (_, constrains) {
+          var sizeOf = MediaQuery.sizeOf(context);
+          return SingleChildScrollView(
+            child: Container(
+              constraints: BoxConstraints(minHeight: constrains.maxHeight),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/images/background_login.png'),
+                    fit: BoxFit.cover),
+              ),
+              child: Center(
+                child: Container(
+                  padding: EdgeInsets.all(40),
+                  width: sizeOf.width * .8,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(16)),
+                  child: Form(
+                    key: formKey,
+                    child: Column(
+                      children: [
+                        Image.asset('assets/images/logo_vertical.png'),
+                        const SizedBox(
+                          height: 48,
+                        ),
+                        Text(
+                          'Bem-vindo!',
+                          style: LabClinicasTheme.titleStyle,
+                        ),
+                        const SizedBox(
+                          height: 48,
+                        ),
+                        TextFormField(
+                          controller: firstNameEC,
+                          validator: Validatorless.required('Nome obrigatório'),
+                          decoration: InputDecoration(
+                            label: Text('Digite o seu nome'),
+                            isDense: true,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 24,
+                        ),
+                        TextFormField(
+                          controller: lastNameEC,
+                          validator:
+                              Validatorless.required('Sobrenome obrigatório'),
+                          decoration: InputDecoration(
+                            label: Text('Digite o seu sobrenome'),
+                            isDense: true,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 24,
+                        ),
+                        SizedBox(
+                          width: sizeOf.width * .8,
+                          height: 48,
+                          child: ElevatedButton(
+                              onPressed: () {
+                                final valid =
+                                    formKey.currentState?.validate() ?? false;
+                                if (valid) {
+                                  selfServiceController.setWhoIAmDataStepAndNext(
+                                      firstNameEC.text, lastNameEC.text);
+                                }
+                              },
+                              child: Text('CONTINUAR')),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          );
+        }),
+      ),
+    );
+  }
+}
