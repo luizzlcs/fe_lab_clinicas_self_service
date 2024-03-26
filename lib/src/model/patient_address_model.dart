fe60d16e53b9d856d43b67ac7690d94f7b78aa63
Luizzlcs <luizzlcs@gmail.com>
2024-03-26 17:14:13 -0300

    Correção do nome Address

--- lib/src/model/patient_address_model.dart
+++ lib/src/model/patient_address_model.dart
@@ -8,17 +8,19 @@ class PatientAddressModel {
     required this.cep,
     required this.streetAddress,
     required this.number,
-    required this.AddressComplement,
+    required this.addressComplement,
     required this.state,
     required this.city,
     required this.district
   });
+
+
   final String cep;
   @JsonKey(name: 'street_address')
   final String streetAddress;
   final String number;
-  @JsonKey(name: 'address_complement')
-  final String AddressComplement;
+  @JsonKey(name: 'address_complement', defaultValue: '')
+  final String addressComplement;
   final String state;
   final String city;
   final String district;
