e8c01d4c12c5d5b696565ca1ffc0ac031f52eb61
Luizzlcs <luizzlcs@gmail.com>
2024-03-26 09:36:46 -0300

    paciente

--- lib/src/modules/self_service/patient/patient_page.dart
+++ lib/src/modules/self_service/patient/patient_page.dart
@@ -1,6 +1,4 @@
-import 'package:fe_lab_clinicas_self_service_cb/src/modules/self_service/self_service_controller.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_getit/flutter_getit.dart';
 
 class PatientPage extends StatelessWidget {
 
@@ -8,7 +6,6 @@ class PatientPage extends StatelessWidget {
 
    @override
    Widget build(BuildContext context) {
-    final patient = Injector.get<SelfServiceController>().debug();
        return Scaffold(
            appBar: AppBar(title: const Text('Patient'),),
            body: Container(),
