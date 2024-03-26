109f6223f9d472654e7bfdd6fecfd2210f4df366
Luizzlcs <luizzlcs@gmail.com>
2024-03-26 09:36:13 -0300

    Criando as rotas par apágina de paciente.

---
+++ lib/src/modules/self_service/find_patient/find_patient_router.dart
@@ -0,0 +1,17 @@
+import 'package:fe_lab_clinicas_self_service_cb/src/modules/self_service/find_patient/find_patient_controller.dart';
+import 'package:fe_lab_clinicas_self_service_cb/src/modules/self_service/find_patient/find_patient_page.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_getit/flutter_getit.dart';
+
+class FindPatientRouter extends FlutterGetItModulePageRouter {
+  const FindPatientRouter({super.key});
+
+  @override
+  List<Bind<Object>> get bindings => [
+        Bind.lazySingleton(
+            (i) => FindPatientController(patientRepository: i())),
+      ];
+
+  @override
+  WidgetBuilder get view => (_) => const FindPatientPage();
+}
