372d9a311c22ce1b33875570b290e1d5d8a999f9
Luizzlcs <luizzlcs@gmail.com>
2024-03-28 15:59:06 -0300

    Ajustando layout da câmera.

--- lib/src/modules/self_service/documents/scan/documents_scan_page.dart
+++ lib/src/modules/self_service/documents/scan/documents_scan_page.dart
@@ -1,8 +1,8 @@
 import 'package:camera/camera.dart';
+import 'package:dotted_border/dotted_border.dart';
 import 'package:fe_lab_clinicas_core/fe_lab_clinicas_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_getit/flutter_getit.dart';
-
 import '../../widget/lab_clinicas_self_service_app_bar.dart';
 
 class DocumentsScanPage extends StatefulWidget {
@@ -76,12 +76,41 @@ class _DocumentsScanPageState extends State<DocumentsScanPage> {
                           child: CircularProgressIndicator(),
                         );
                       case AsyncSnapshot(connectionState: ConnectionState.done):
-                        if (cameraController.value.isInitialized){
-                          return CameraPreview(cameraController);
+                        if (cameraController.value.isInitialized) {
+                          return ClipRRect(
+                            borderRadius: BorderRadius.circular(16),
+                            child: SizedBox(
+                              width: sizeOf.width * .5,
+                              child: CameraPreview(
+                                cameraController,
+                                child: DottedBorder(
+                                    dashPattern: [1, 10, 1, 3],
+                                    borderType: BorderType.RRect,
+                                    strokeWidth: 4,
+                                    color: LabClinicasTheme.orangeColor,
+                                    strokeCap: StrokeCap.square,
+                                    radius: Radius.circular(16),
+                                    child: SizedBox.expand()),
+                              ),
+                            ),
+                          );
                         }
                     }
-                    return Center(child: Text('Erro ao carregar câmera.'),);
+                    return Center(
+                      child: Text('Erro ao carregar câmera.'),
+                    );
                   },
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.camera,
+                    color: LabClinicasTheme.blueColor,
+                    size: 60,
+                  ),
                 )
               ],
             ),
@@ -90,4 +119,7 @@ class _DocumentsScanPageState extends State<DocumentsScanPage> {
       ),
     );
   }
+
+  @override
+  List<Object?> get props => [cameraController];
 }
